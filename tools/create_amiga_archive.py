4c6cf658652fb47940a21f34225625daab152f89
Jean-François Fabre <jotd666@gmail.com>
2026-04-28 23:18:22 +0200

    missing script added

---
+++ tools/create_amiga_archive.py
@@ -0,0 +1,38 @@
+import subprocess,os,glob,shutil,pathlib
+
+
+progdir = pathlib.Path(os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir)))
+
+gamename = "gyruss"
+# JOTD path for cranker, adapt to whatever your path is :)
+os.environ["PATH"] += os.pathsep+r"K:\progs\cli"
+
+cmd_prefix = ["make","-f",str(progdir / "makefile.am")]
+
+subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
+
+subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd=progdir / "src")
+# create archive
+
+outdir = progdir / f"{gamename}_HD"
+
+if outdir.exists():
+    for x in outdir.glob("*"):
+        os.remove(x)
+else:
+    outdir.mkdir()
+for file in ["readme.md"]:  #,
+    shutil.copy(os.path.join(progdir,file),outdir)
+
+shutil.copy(os.path.join(progdir,"assets","amiga","Gyruss.info"),outdir)
+
+
+for suffix in ["_1mb","_2mb"]:
+    exename = gamename+suffix
+    slavename = exename + ".slave"
+
+    shutil.copy(os.path.join(progdir,exename),outdir)
+    shutil.copy(os.path.join(progdir,slavename),outdir)
+    subprocess.check_output(["cranker_windows.exe","-f",os.path.join(progdir,exename),"-o",os.path.join(progdir,f"{exename}.rnc")])
+
+subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
